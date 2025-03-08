bb22a7bcd4049dc70cdeb0699593fcbee1602736
CANARIOag <90974155+CANARIOag@users.noreply.github.com>
2025-03-08 14:42:02 -0300

    Update Program.cs

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -29,4 +29,11 @@ builder.Services.AddAuthorizationCore();
 
 builder.Services.AddScoped<CustomHttpHandler>();
 
+//Agrego los servicios
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<BillService>();
+builder.Services.AddScoped<SupplierService>();
+builder.Services.AddScoped<OrderService>();
+builder.Services.AddScoped<InventoryService>();
+
 await builder.Build().RunAsync();
